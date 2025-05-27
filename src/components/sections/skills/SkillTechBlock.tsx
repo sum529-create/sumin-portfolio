479bdfcea078806b6db15d5d8f455c1b4033a302
노수민 <nosumin29@gmail.com>
2025-05-27 16:49:46 +0900

    ✨ update : SkillTechBlockProps의 color 타입을 문자열 리터럴로 변경 #4

--- src/components/sections/skills/SkillTechBlock.tsx
+++ src/components/sections/skills/SkillTechBlock.tsx
@@ -2,21 +2,33 @@ import { ReactNode } from 'react';
 
 interface SkillTechBlockProps {
   children: ReactNode;
-  color: string;
+  color: 'cyan' | 'green' | 'purple';
   text: string;
 }
 const SkillTechBlock = ({ children, color, text }: SkillTechBlockProps) => {
+  const colorTextVariants = {
+    cyan: 'text-cyan-400',
+    green: 'text-green-400',
+    purple: 'text-purple-400',
+  };
+  const colorBgVariants = {
+    cyan: 'bg-cyan-400',
+    green: 'bg-green-400',
+    purple: 'bg-purple-400',
+  };
+
   return (
     <div className='space-y-3'>
       <div className='mb-4 text-center'>
-        <h3 className={`text-base font-semibold text-${color}-400 md:text-lg`}>
+        <h3
+          className={`text-base font-semibold ${colorTextVariants[color]} md:text-lg`}
+        >
           {text}
         </h3>
         <div
-          className={`mx-auto mt-2 h-0.5 w-12 rounded-full bg-${color}-400`}
+          className={`mx-auto mt-2 h-0.5 w-12 rounded-full ${colorBgVariants[color]}`}
         ></div>
       </div>
-
       {children}
     </div>
   );
