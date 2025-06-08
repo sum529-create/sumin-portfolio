b44205a1c7417268635691e62e060cfa7c951685
노수민 <nosumin29@gmail.com>
2025-06-08 20:21:54 +0900

    ✨ update : BlogCard 컴포넌트에 이미지 로딩 오류 처리 및 블러 데이터 URL 추가 #6

--- src/components/sections/blogs/BlogCard.tsx
+++ src/components/sections/blogs/BlogCard.tsx
@@ -83,6 +83,11 @@ const BlogCard = ({ blog, i }: BlogCardProps) => {
             fill={true}
             sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
             className='object-cover'
+            onError={(e) => {
+              e.currentTarget.src = '/blog/blog-placeholder.jpg';
+            }}
+            placeholder='blur'
+            blurDataURL='data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAABAAEDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFQEBAQAAAAAAAAAAAAAAAAAAAAX/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwCdABmX/9k='
           />
         </div>
         <div className='p-6'>
