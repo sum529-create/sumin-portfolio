514e8e8ded1a5c3698a91e879054304662f711f1
노수민 <nosumin29@gmail.com>
2025-06-09 15:46:43 +0900

    💄 style : project 하단 간격 추가 #3

--- src/components/sections/ProjectSection.tsx
+++ src/components/sections/ProjectSection.tsx
@@ -6,7 +6,7 @@ import ProjectCard from './projects/ProjectCard';
 export default function ProjectSection() {
   return (
     <section id='projects' className='relative min-h-screen py-20'>
-      <div className='px-4pb-[100px] container mx-auto max-w-full'>
+      <div className='container mx-auto max-w-full px-4 pb-40'>
         <SectionTitle
           title='Projects'
           subTitle='나의 프로젝트들'
