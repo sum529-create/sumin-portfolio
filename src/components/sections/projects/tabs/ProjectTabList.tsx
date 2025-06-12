285bce48c9fd89848990c86a1202a1cbacbca5c9
노수민 <nosumin29@gmail.com>
2025-06-13 00:53:02 +0900

    🎉 add : ProjectTabList 컴포넌트에 아이콘 추가 및 기존 이모지 대체 #3

--- src/components/sections/projects/tabs/ProjectTabList.tsx
+++ src/components/sections/projects/tabs/ProjectTabList.tsx
@@ -1,4 +1,5 @@
 import { TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { BrickWallFire, Lightbulb, NotebookPen, Wrench } from 'lucide-react';
 
 const ProjectTabList = () => {
   return (
@@ -8,28 +9,36 @@ const ProjectTabList = () => {
         className='flex items-center gap-2 rounded-lg text-white/70 transition-all duration-200 data-[state=active]:bg-white/10 data-[state=active]:text-white data-[state=active]:shadow-md'
       >
         <span className='hidden sm:inline'>개요</span>
-        <span className='sm:hidden'>📋</span>
+        <span className='sm:hidden'>
+          <NotebookPen />
+        </span>
       </TabsTrigger>
       <TabsTrigger
         value='tech'
         className='flex items-center gap-2 rounded-lg text-white/70 transition-all duration-200 data-[state=active]:bg-white/10 data-[state=active]:text-white data-[state=active]:shadow-md'
       >
         <span className='hidden sm:inline'>기술스택</span>
-        <span className='sm:hidden'>🔧</span>
+        <span className='sm:hidden'>
+          <Wrench />
+        </span>
       </TabsTrigger>
       <TabsTrigger
         value='retrospective'
         className='flex items-center gap-2 rounded-lg text-white/70 transition-all duration-200 data-[state=active]:bg-white/10 data-[state=active]:text-white data-[state=active]:shadow-md'
       >
         <span className='hidden sm:inline'>회고</span>
-        <span className='sm:hidden'>💭</span>
+        <span className='sm:hidden'>
+          <Lightbulb />
+        </span>
       </TabsTrigger>
       <TabsTrigger
         value='blog'
         className='flex items-center gap-2 rounded-lg text-white/70 transition-all duration-200 data-[state=active]:bg-white/10 data-[state=active]:text-white data-[state=active]:shadow-md'
       >
         <span className='hidden sm:inline'>트러블슈팅</span>
-        <span className='sm:hidden'>🚨</span>
+        <span className='sm:hidden'>
+          <BrickWallFire />
+        </span>
       </TabsTrigger>
     </TabsList>
   );
